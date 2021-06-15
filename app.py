6521014f1d3986294402d4eef0cf4f4b835603d3
kanishkaverma26 <80206952+kanishkaverma26@users.noreply.github.com>
2021-06-15 21:46:07 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,29 @@
+import streamlit as st 
+import os
+from textblob import TextBlob
+# Title
+st.title("Sentiment Analysis")
+result_sentiment=0
+# Sentiment Analysis
+st.subheader("Analyse Your Text")
+message = st.text_area("Enter Text")
+
+if st.button("Analyze"):
+      blob = TextBlob(message)
+      result_sentiment=blob.sentiment[0]
+      
+    
+if result_sentiment > 0.5 :
+      st.write('Prediction  :')
+      st.subheader('Positive')
+      st.success(f'Score : {result_sentiment}')
+
+elif result_sentiment < 0 :
+      st.write('Prediction  :')
+      st.subheader('Negative')
+      st.success(f'Score : {result_sentiment}')
+
+else :
+      st.write('Prediction  :')
+      st.subheader('Neutral')
+      st.success(f'Score : {result_sentiment}')
